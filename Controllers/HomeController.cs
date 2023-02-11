55de591b377dce21021f2df242a3fced07952c87
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2023-02-11 18:24:22 -0300

    Exibindo os produtos preferidos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,31 @@
 ﻿using EcommerceMVC.Models;
+using EcommerceMVC.Repositories.Interfaces;
+using EcommerceMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace EcommerceMVC.Controllers {
     public class HomeController : Controller {
 
+        //Instancia do repositorio de produtos
+        private readonly IProdutoRepository _produtoRepository;
+
+        public HomeController(IProdutoRepository produtoRepository) {
+            _produtoRepository = produtoRepository;
+        }
+
         public IActionResult Index() {
-            return View();
+            var homeViewModel = new HomeViewModel {
+                //Produtos obtidos pelo repositorio
+                ProdutosPreferidos = _produtoRepository.ProdutosPreferidos
+            };
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using EcommerceMVC.Models;
+
+namespace EcommerceMVC.ViewModels {
+    public class HomeViewModel {
+
+        //Exibir os produtos preferidos
+        public IEnumerable<Produto> ProdutosPreferidos { get; set; }
+    }
+}
