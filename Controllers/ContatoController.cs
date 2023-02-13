5460374b1c7eb85bdd47d5a72fc76a405f915bf9
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2023-02-13 20:31:43 -0300

    Criando uma TagHelper email

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.Controllers {
+    public class ContatoController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
