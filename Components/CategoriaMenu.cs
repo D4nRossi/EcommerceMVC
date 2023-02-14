7bf8c5252f9baf605576faa46490a841b06fb214
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2023-02-14 11:01:08 -0300

    Criar View Componente para exibir as categorias no menu Produtos

---
+++ Components/CategoriaMenu.cs
@@ -0,0 +1,18 @@
+﻿using EcommerceMVC.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.Components {
+    public class CategoriaMenu : ViewComponent{
+
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository) {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke() {
+            var categorias = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
