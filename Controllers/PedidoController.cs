518611824b03abd7434e9d6ec246dd334a7671d8
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2023-02-14 21:41:16 -0300

    Criando o controlador PedidoController

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,26 @@
+﻿using EcommerceMVC.Models;
+using EcommerceMVC.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.Controllers {
+    public class PedidoController : Controller {
+        //Acessar e criar os pedidos
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra) {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout() {
+            return View();  
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido) {
+            return View();
+        }
+    }
+}
