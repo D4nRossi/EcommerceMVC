9ceaaa28c70b8af16d489758f585387203757ec8
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2023-02-28 13:12:13 -0300

    Exibindo as opções para Upload e Consulta de imagens

---
+++ Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,26 @@
+﻿using EcommerceMVC.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace EcommerceMVC.Areas.Admin.Controllers {
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminImagensController : Controller {
+
+        //Obter o nome da pasta onde salvar os arquivos
+        private readonly ConfigurationImagens _myConfig;
+        //Info do ambiente onde a aplicação esta rodando
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        //CTT injetando as duas instancias
+        public AdminImagensController(IWebHostEnvironment hostEnvironment, IOptions<ConfigurationImagens> myConfiguration) {
+            _hostingEnvironment = hostEnvironment;
+            _myConfig = myConfiguration.Value;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
